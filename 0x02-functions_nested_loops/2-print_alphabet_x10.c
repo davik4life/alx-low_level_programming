64c1370f1c37622bf520cb5e5493e0d34ede2976
Victor Adeshile <davik4life@gmail.com>
2023-03-21 10:52:19 -0700

    Betty Fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@
 
 void print_alphabet_x10(void)
 {
-        for(int i = 0; i < 10; i++)
-        {
-                char j;
-                for(j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		char j;
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
 		_putchar('\n');
-        }
+	}
 
 }
