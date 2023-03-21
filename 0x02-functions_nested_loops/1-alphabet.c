fcb3561306afef8250289896188cf95cc6d0641c
Victor Adeshile <davik4life@gmail.com>
2023-03-21 10:30:30 -0700

    Refactored all the codes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,11 @@
 
 void print_alphabet(void)
 {       
-        char i;
-        for(i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
-        return;
+	char i;
+	for(i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
 }
-
