2d61e1792bdac8d749f385a583a68ae46a4b916a
Victor Adeshile <davik4life@gmail.com>
2023-03-21 10:40:57 -0700

    Bug fixes with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  * Return: Always 0.
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char i;
 
-	for(i = 97; i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
