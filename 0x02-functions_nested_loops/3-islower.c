f834cf41401bf9224b8fd6f08ca2a143452abd6d
Victor Adeshile <davik4life@gmail.com>
2023-03-21 11:18:01 -0700

    islower code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
 /**
  * _islower - Checks for lowercase Character.
  *
+ * c: This should be computed using ASCII numbers
+ *
  * Return: 0 otherwise.
  *
  */
@@ -11,7 +13,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
