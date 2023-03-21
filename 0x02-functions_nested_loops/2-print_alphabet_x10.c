d935eba9bd6034a1e67ca2be57c0c9ba85b7711a
Victor Adeshile <davik4life@gmail.com>
2023-03-21 10:56:16 -0700

    Bug Fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10() Prints 10 times alphabet in lowercase
+ * print_alphabet_x10() - Prints 10 times alphabet in lowercase
  *
  * Return 0;
  *
@@ -11,15 +11,16 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		char j;
+
 		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 
 }
