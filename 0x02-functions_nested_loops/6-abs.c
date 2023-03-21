4353511fbdd22805d3ba2cc84605c4362e30b889
Victor Adeshile <davik4life@gmail.com>
2023-03-21 13:48:17 -0700

    Absolute Value.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,18 @@
 /**
  * _abs: Computes the absolute value of an integer.
  *
+ * @n: This is the integer to be calculated.
+ *
+ * Return: Returns the absolute value of the given integer and feeds the value 
+ * to the main from where it is been called. 
+ *
  */
 
 int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return n;
+		return (n);
 	}
 	return (n * -1);
 }
