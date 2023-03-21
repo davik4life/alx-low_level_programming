07a6917ac11b89091727a05d3745880ba6ebc20d
Victor Adeshile <davik4life@gmail.com>
2023-03-21 16:46:01 -0700

    Addition Task 10.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,10 @@
 /**
  * add - Adds two integers together to provide the sum.
  *
+ * @y: One of the integer place holders in the parameters.
+ *
+ * @z: Here's the second integer placeholder in the parameters.
+ *
  * Return: The value of the sum of two integers is then returned.
  */
 
