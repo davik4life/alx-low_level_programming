958f80089aedca7db066b6f396b3ae1c63a7b58d
Victor Adeshile <davik4life@gmail.com>
2023-03-21 02:30:12 -0700

    Print _putchar program.

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main -  Prints _putchar followed by a new line.
+ *
+ * Return 0
+ */
+
+int main()
+{
+	printf("_putchar\n");
+	return 0;
+}
---
+++ 0x02-function_nested_loops/README.md
@@ -0,0 +1,1 @@
+Here's a readme for the Nested Loop Section.
