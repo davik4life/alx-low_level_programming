0d5d482aca37a6bb0ab4569bce68a43d016c6c13
Victor Adeshile <davik4life@gmail.com>
2023-03-23 02:32:45 -0700

    added the files 0-main.c and also main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - This calls the function that prints both positive and negative values.
+ * 
+ * Return: 0.
+*/
+
+int main(void)
+{
+	int i;
+	/* Ensure to set the variable to test with 0. */
+	i = 0;
+	/* Testing with the value i. */
+	
+	positive_or_negative(i);
+	return (0);
+}
