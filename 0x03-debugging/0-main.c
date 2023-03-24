67bdcf6506abeca6e52d18c6180f748b3cd12af8
Victor Adeshile <davik4life@gmail.com>
2023-03-23 18:07:59 -0700

    REfactored teh code.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - This calls the function that prints both positive and negative values
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
-*/
+ */
 
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
