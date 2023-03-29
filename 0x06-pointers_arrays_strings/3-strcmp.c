a35ef7504f38b7b0098b4313ebb714f7f1159020
Victor Adeshile <davik4life@gmail.com>
2023-03-29 16:43:33 -0700

    Completed Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Functiion compares two strings
+ * @s1: input parameter
+ * @s2: input parameter
+ *
+ * Return: 0 if equal or b if otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+
+	while (s1[a] != '\0' && b == 0)
+	{
+		b = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
+}
