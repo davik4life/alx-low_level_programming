3323bb180d3d341d19ec3dc7157ecd4cfdfdbaca
Victor Adeshile <davik4life@gmail.com>
2023-04-03 17:21:14 -0700

    Strspn updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,12 +2,11 @@
 
 /**
  * _strspn - this function gets the length of a prefix substring
- *
  * @s: this is the segment to return bytes from
  * @accept: this is the bytes to include
  *
  * Return: this is the number of bytes in the initial segment of @s which 
- * consist only of bytes from @accept
+ * consist only of bytes from @accept.
  */
 
 unsigned int _strspn(char *s, char *accept)
